a50ef6f75e0dcdcefc4e515b92e5620c166dc0fa
nicovill90 <nicovillalonga90@hotmail.com>
2016-12-28 22:34:25 -0300

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -18,5 +18,16 @@ module.exports = {
     }
 
     return $.isArray(todos) ? todos : [];
+  },
+
+  filterTodos: function (todos, showCompleted, searchText) {
+    let filteredTodos = todos;
+
+    filteredTodos = filteredTodos
+      .filter((todo) => !todo.completed || showCompleted)
+      .filter((todo) => searchText.length === 0 || todo.text.toLowerCase().indexOf(searchText) > -1)
+      .sort((a, b) => a.completed - b.completed);
+
+    return filteredTodos;
   }
 };
